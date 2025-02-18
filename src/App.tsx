6ea19417607b28dfcff0dead8a40f2c9dd59e57a
NYeonK <skdus728@gmail.com>
2025-02-19 00:23:11 +0900

    [ feat ] /apply 접근 시, /apply/consent로 redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/@common/Layout";
 import HomePage from "./components/HomePage";
 import ApplyPage from "./components/ApplyPage";
@@ -13,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="apply" element={<ApplyPage />}>
+          <Route index element={<Navigate to="consent" replace />} />
           <Route path="consent" element={<PrivacyConsent />} />
           <Route path="info" element={<ApplyInfo />} />
           <Route path="job" element={<ApplyJob />} />
