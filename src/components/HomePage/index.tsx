2d5f4e1a921743011fd24fa9661ccffb982008e5
NYeonK <skdus728@gmail.com>
2025-02-18 23:03:22 +0900

    [ feat ] add logo image

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <section className="h-full bg-white rounded-2xl">
+      <img src="/src/assets/logo.webp" alt="로고" />
       <Link to="/apply/consent">지원하기</Link>
     </section>
   );
