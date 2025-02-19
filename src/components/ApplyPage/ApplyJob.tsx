2cb22ffd984961b1562d44ebdf78c956e82ce53e
NYeonK <skdus728@gmail.com>
2025-02-20 00:26:51 +0900

    [ feat ] ApplyJob component UI 구현

--- src/components/ApplyPage/ApplyJob.tsx
+++ src/components/ApplyPage/ApplyJob.tsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import Radio from "../@common/Radio";
+
+type JobType = "Fe" | "Be" | "Design" | "Ios" | "Android" | "Po" | null;
+
+const JOBS = [
+  { value: "Fe", label: "프론트엔드" },
+  { value: "Be", label: "백엔드" },
+  { value: "Design", label: "디자인" },
+  { value: "Ios", label: "iOS" },
+  { value: "Android", label: "안드로이드" },
+  { value: "Po", label: "Product Owner" },
+] as const;
+
 const ApplyJob = () => {
-  return <div>ApplyJob</div>;
+  const [job, setJob] = useState<JobType>(null);
+
+  const handleJobChange = (selectedJob: JobType) => {
+    setJob(job === selectedJob ? null : selectedJob);
+  };
+
+  return (
+    <div className="flex flex-col gap-4 py-5 px-4 border border-gray-200 font-semibold rounded-lg">
+      <p>
+        지원 분야를 선택해주세요
+        <span className="pl-1 text-red-500">*</span>
+      </p>
+      <div className="flex flex-col gap-4">
+        {JOBS.map(({ value, label }) => (
+          <Radio
+            checked={job === value}
+            onChange={() => handleJobChange(value)}
+            label={label}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ApplyJob;
