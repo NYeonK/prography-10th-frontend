91f78cc4566531588d00d03815327fcad35942e0
NYeonK <skdus728@gmail.com>
2025-02-20 22:46:44 +0900

    [ refactor ] utils 폴더 생성 및 STEPS 정보 이동

---
+++ src/utils/apply-steps.ts
@@ -0,0 +1,21 @@
+export const APPLY_STEPS = [
+  {
+    id: 1,
+    path: "/apply/consent",
+    title: "개인정보 수집 동의",
+    content:
+      "프로그라피 10기 지원을 위한 개인정보 수집에 대한 동의가 필요합니다",
+  },
+  {
+    id: 2,
+    path: "/apply/info",
+    title: "기본 정보",
+    content: "연락 가능한 정보를 입력해주세요",
+  },
+  {
+    id: 3,
+    path: "/apply/job",
+    title: "지원 정보",
+    content: "지원하고자 하는 분야를 선택해주세요",
+  },
+];
